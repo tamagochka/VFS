81508f647dc2acf07ae67312d305cdfdffa1cf7b
tamagochka <tamagochka@gmail.com>
2018-06-04 09:48:28 +0300

    Initial commit. Created class Entity.

---
+++ src/main/java/my/tamagochka/VFS/Entity.java
@@ -0,0 +1,11 @@
+package my.tamagochka.VFS;
+
+public interface Entity {
+    void create();
+    void delete();
+    void rename();
+    void copy();
+    void move();
+    boolean isExist();
+    String getAbsolutePath();
+}
---
+++ src/main/java/my/tamagochka/VFS/Main.java
@@ -0,0 +1,30 @@
+package my.tamagochka.VFS;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+/*
+        Filesystem fs = new Filesystem("test");
+        System.out.println(fs.isExist("."));
+        System.out.println(fs.isExist("123.txt"));
+        System.out.println(fs.getAbsolutePath("."));
+        Iterator<String> i = fs.getFileIterator(".", true);
+        while(i.hasNext()) {
+            System.out.println(i.next());
+        }
+*/
+
+
+
+
+
+
+
+
+
+    }
+}
